7eacf984d1969466c018085af0e1ec45b5334236
= <miguelestanga13@gmail.com>
2022-10-23 12:07:35 -0400

    version1

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-console.clear();
+
 require('./src/util/conexion');
 const express = require('express');
 const cors  = require('cors');
 const fileupload = require('express-fileupload')
-const expressip = require('express-ip')
+
 const router = require('./src/router/index');
 const  app = express();
 
 
 const port = process.env.PORT || 4000
 
-app.use(expressip().getIpInfoMiddleware)
+
 app.use(express.json());
 app.use(cors());
 app.use(fileupload({
@@ -20,5 +20,5 @@ app.use(fileupload({
 app.use(router);
 
 app.listen(port , function(){
-    console.log(`app lista en el puesto ${puerto}`);
+    console.log(`app lista en el puesto ${port}`);
 })
--- src/models/monitoreo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-const {Schema , model } = mongoose
-
-const Monitoreomodel = new Schema({
-    
-    ip:{type:String},
-    Usuatio:{type:[]}
-})
-module.exports = model('Monitor' , Monitoreomodel)
